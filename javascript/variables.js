a11067a03edc4faba0fc647b2edbf816989af615
Nitin <93211438+nitinsisodia920@users.noreply.github.com>
2024-07-05 17:45:21 +0000

    variables

---
+++ javascript/variables.js
@@ -0,0 +1,11 @@
+const accountId = 123456
+let accountEmail = "nitinrajput9250@gmail.com"
+var accountPassword = "12345"
+accountCity = "delhi"
+let userState
+
+//accountId = 123458
+accountPassword = "456789"
+console.log(accountId)
+console.log(accountPassword)
+console.table([accountId , accountEmail , accountPassword , accountCity , userState])
