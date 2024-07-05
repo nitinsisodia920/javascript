9f24faa1f2a849e281caa3c5cca88a097017d715
Nitin <93211438+nitinsisodia920@users.noreply.github.com>
2024-07-05 18:20:42 +0000

    datatypes

---
+++ javascript/datatypes.js
@@ -0,0 +1,19 @@
+"using strict" //treat all JS code as newer version
+
+// alert( 3 + 3) we are using nodejs, not browser
+
+//non primitive dataTypes 
+/*
+number => range 2 to 52
+bigint => use for big number
+boolean => true/false
+null => standalone vlaue
+undefined
+symbol => unique
+*/ 
+
+// primitive dataTypes
+
+/*
+object
+ */
