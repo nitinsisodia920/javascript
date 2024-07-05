efc9ab2ef892a08f1578893f06d48b3f95ce5a09
Nitin <93211438+nitinsisodia920@users.noreply.github.com>
2024-07-05 18:55:34 +0000

    conversion of datatypes

---
+++ javascript/conversionOperation.js
@@ -0,0 +1,26 @@
+/* let score = "33a" //not a number
+let name = "nitin"
+
+console.log(typeof score)
+console.log(typeof (score))
+console.log(typeof name)
+ 
+//conversion string to number
+
+let valueInNumber = Boolean(score)
+
+//print
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber) */
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let someNumber = 45
+let stringConversion = String(someNumber)
+
+console.log(stringConversion)
+console.log(typeof stringConversion)
