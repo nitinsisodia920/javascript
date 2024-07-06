8089597624a1ec83658857f46a5a76225b16aff3
Nitin <93211438+nitinsisodia920@users.noreply.github.com>
2024-07-06 09:34:27 +0000

    conversion datatypes

--- javascript/conversionOperation.js
+++ javascript/conversionOperation.js
@@ -17,12 +17,49 @@ console.log(valueInNumber) */
 let isLoggedIn = ""
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 let someNumber = 45
 let stringConversion = String(someNumber)
 
-console.log(stringConversion)
-console.log(typeof stringConversion)
+/* console.log(stringConversion)
+console.log(typeof stringConversion) */
 
 // ********************** Operation **********************
+
+let value = 5
+let negvalue = -value
+
+//console.log(negvalue)
+
+//console.log( 2 + 2)
+//console.log( 2 - 2)
+//console.log( 2 * 2)
+//console.log( 2 / 2)
+//console.log( 2 ** 2)
+//console.log( 2 % 2)
+
+let str1 = "hello"
+let str2 = " Nitin"
+
+let str3 = str1 + str2
+/* console.log(str3)
+
+console.log( "3" + 3)
+console.log( "3" + "3")
+console.log( 3 + 3)
+console.log( "3" + 3 + 3)
+console.log( 3 + 3 + "3") */
+
+//console.log(+true)
+//console.log(+"")
+
+let num1,num2,num3 
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+gameCounter++;
+
+console.log(gameCounter)
